882a15dccade1c6c9808240cede6acf89da0237e
anthonyRanivoarison <antonitsiory@gmail.com>
2025-08-03 16:38:36 +0300

    feat:added banner intro, kind of page header

---
+++ src/components/templates/BannerIntro.tsx
@@ -0,0 +1,10 @@
+const BannerIntro = () => (
+  <div className="flex items-center justify-center flex-col leading-8 py-8">
+    <h1 className="text-5xl font-bold my-2">Pokédex</h1>
+    <p className="text-gray-500">
+      Explorez l'univers fascinant des Pokémons et découvrez leurs caractéristiques uniques.
+    </p>
+  </div>
+);
+
+export default BannerIntro;
