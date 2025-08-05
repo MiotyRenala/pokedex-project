8ece0d23a93045d11469c62a9bf35b6df3fb75b5
Miora Andrian <miora.andri@icloud.com>
2025-08-05 15:35:06 +0300

    feat: adding a filter dropdown button

---
+++ src/components/search/FilterDropdown.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+interface FilterDropdownProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const FilterDropdown: React.FC<FilterDropdownProps> = ({ value, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleSelect = (filter: string) => {
+    onChange(filter);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <button
+          type="button"
+          onClick={toggleDropdown}
+          className="inline-flex justify-center items-center w-full h-[60px] px-4 py-2 text-gray-500 bg-gray-100 border-2 border-transparent rounded-[10px] hover:border-blue-100 focus:outline-none"
+        >
+          Filter: {value === "" ? "None" : value}
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="absolute z-10 mt-2 w-full bg-white border border-gray-200 rounded-md shadow-lg">
+          <button
+            className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            onClick={() => handleSelect("")}
+          >
+            None
+          </button>
+          <button
+            className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            onClick={() => handleSelect("A-Z")}
+          >
+            A - Z
+          </button>
+          <button
+            className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            onClick={() => handleSelect("Z-A")}
+          >
+            Z - A
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FilterDropdown;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,48 @@
 import BannerIntro from "@/components/templates/BannerIntro.tsx";
 import InputSearch from "@/components/search/InputSearch";
+import FilterDropdown from "@/components/search/FilterDropdown";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import type { Pokemon } from "@/types/types";
 
 const HomePage = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
+  const [sortFilter, setSortFilter] = useState("");
+  
+  const typeColor: { [key: string]: string } = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=10"
-      );
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
       const results = response.data.results;
       const detailedData = await Promise.all(
         results.map(async (pokemon: { url: string }) => {
           const res = await axios.get(pokemon.url);
           return {
             id: res.data.id,
-            name: res.data.name,
-            types: res.data.types.map((t: any) => t.type.name).join(", "),
-            sprites: res.data.sprites.front_default,
+            name: res.data.name[0].toUpperCase() + res.data.name.slice(1),
+            types: res.data.types.map((t: string[]) => t.type.name),
+            sprites: res.data.sprites.other["official-artwork"].front_default,
           };
         })
       );
@@ -33,27 +54,68 @@ const HomePage = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  
-  const filteredPokemon = pokemonList.filter((pokemon) =>
+
+  let filteredPokemon = pokemonList.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
   );
-
+  
+  if (sortFilter === "A-Z") {
+    filteredPokemon.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortFilter === "Z-A") {
+    filteredPokemon.sort((a, b) => b.name.localeCompare(a.name));
+  } else {
+    filteredPokemon.sort((a, b) => a.id - b.id);
+  }
+  
   return (
     <>
-      <div className="bg-white">
+      <div className="bg-white flex flex-col items-center">
         <BannerIntro />
-        <InputSearch value={searchValue} onChange={setSearchValue} />
+
+        <div className="flex flex-row justify-center gap-2 w-[50vw] h-[20vh]">
+          <InputSearch value={searchValue} onChange={setSearchValue} />
+          <FilterDropdown value={sortFilter} onChange={setSortFilter} />
+        </div>
+
         <hr />
-        <div className="grid grid-cols-4 gap-2.5 ml-5 ">
+        <div className="grid grid-cols-4 gap-2.5 ">
           {filteredPokemon.map((pokemon) => (
-            <div className="flex-col justify-center items-center  border-2 " key={pokemon.id}>
-              <p>{pokemon.id}</p>
-              <img src={pokemon.sprites} alt="" />
-              <div>
-                <strong>{pokemon.name}</strong>
-                <p>{pokemon.types}</p>
+            <a href="http://localhost:5173/pokemon/:id"
+              onClick={"window.location.href='http://localhost:5173/pokemon/:id';"}>
+              
+            <div
+              className="flex-col justify-center w-50 rounded-xl"
+              style={{ backgroundColor: typeColor[pokemon.types[0]] }}
+             
+            >
+              <div className="flex flex-col items-center p-3  ">
+                {pokemon.id<10 && (
+                  <p className="text-xs self-end bg-black bg-opacity-20 px-2 rounded-2xl">#00{pokemon.id}</p>
+
+                )}
+                {pokemon.id>9 && (
+                  <p className="text-xs self-end bg-black bg-opacity-20 px-2 rounded-2xl">#0{pokemon.id}</p>
+                )}
+                {pokemon.id>99 && (
+                  <p className="text-xs self-end bg-black bg-opacity-20 px-2 rounded-2xl">#{pokemon.id}</p>
+                )}
+                <img src={pokemon.sprites} alt="" className="w-30" />
+                <div className="flex flex-col items-center ">
+                  <strong>{pokemon.name}</strong>
+                  <div className="flex gap-1">
+                  <p className="p-1 px-2 text-white font-bold shadow-lg shadow-black-500/100 border-1  rounded-2xl text-xs"
+                   style={{ backgroundColor: typeColor[pokemon.types[0]]}}>
+                    {pokemon.types[0]}
+                  </p>
+                  {pokemon.types[1] && (
+                    <p className="p-1 px-2 text-white font-bold shadow-lg shadow-black-500/100 border-1  rounded-2xl text-xs"
+                     style={{ backgroundColor: typeColor[pokemon.types[1]]}}> {pokemon.types[1]}</p>
+                  )}
+                  </div>
+                </div>
               </div>
             </div>
+            </a>
           ))}
         </div>
       </div>
