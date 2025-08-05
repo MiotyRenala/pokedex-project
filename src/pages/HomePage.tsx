f97ee93c4e30b56241237be782a0207444b1e488
N0mena <alida.rajaonarivelo@gmail.com>
2025-08-05 12:49:51 +0300

    feat: link forward to PokemonPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import type { Pokemon } from "@/types/types";
 
 const HomePage = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  
   const typeColor: { [key: string]: string } = {
     normal: "#A8A77A",
     fire: "#EE8130",
@@ -38,7 +39,7 @@ const HomePage = () => {
             id: res.data.id,
             name: res.data.name[0].toUpperCase() + res.data.name.slice(1),
             types: res.data.types.map((t: string[]) => t.type.name),
-            sprites: res.data.sprites.front_default,
+            sprites: res.data.sprites.other["official-artwork"].front_default,
           };
         })
       );
@@ -59,9 +60,13 @@ const HomePage = () => {
         <hr />
         <div className="grid grid-cols-4 gap-2.5 ">
           {pokemonList.map((pokemon) => (
+            <a href="http://localhost:5173/pokemon/:id"
+              onClick={"window.location.href='http://localhost:5173/pokemon/:id';"}>
+              
             <div
-              className="flex-col justify-center w-50 rounded-xl "
+              className="flex-col justify-center w-50 rounded-xl"
               style={{ backgroundColor: typeColor[pokemon.types[0]] }}
+             
             >
               <div className="flex flex-col items-center p-3  ">
                 {pokemon.id<10 && (
@@ -71,7 +76,10 @@ const HomePage = () => {
                 {pokemon.id>9 && (
                   <p className="text-xs self-end bg-black bg-opacity-20 px-2 rounded-2xl">#0{pokemon.id}</p>
                 )}
-                <img src={pokemon.sprites} alt="" />
+                {pokemon.id>99 && (
+                  <p className="text-xs self-end bg-black bg-opacity-20 px-2 rounded-2xl">#{pokemon.id}</p>
+                )}
+                <img src={pokemon.sprites} alt="" className="w-30" />
                 <div className="flex flex-col items-center ">
                   <strong>{pokemon.name}</strong>
                   <div className="flex gap-1">
@@ -87,6 +95,7 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
+            </a>
           ))}
         </div>
       </div>
