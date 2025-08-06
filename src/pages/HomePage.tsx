3e86ecfc470246ebaf6c5ccce12b7ed4d1a0982c
anthonyRanivoarison <antonitsiory@gmail.com>
2025-08-06 12:59:15 +0300

    feat:home page refactor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,19 @@
 import BannerIntro from "@/components/templates/BannerIntro.tsx";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
-import type { Pokemon } from "@/types/types";
+import type {Pokemon} from "@/types/types";
 import Pagination from "@/components/templates/Pagination.tsx";
-import { PokemonCard } from "@/components/ui/PokemonCard.tsx";
-import { useSearchParams } from "react-router-dom";
+import {PokemonCard} from "@/components/ui/PokemonCard.tsx";
+import {useSearchParams} from "react-router-dom";
 
 const HomePage = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [searchValue, setSearchValue] = useState("");
   const [sortFilter, setSortFilter] = useState("");
-  
+  const [totalPages, setTotalPages] = useState(0);
+  const [ searchParams ] = useSearchParams();
+  const currentPage = parseInt(searchParams.get("page") || "1", 10);
+
   const typeColor: { [key: string]: string } = {
     normal: "#A8A77A",
     fire: "#EE8130",
@@ -32,84 +35,51 @@ const HomePage = () => {
     fairy: "#D685AD",
   };
 
-  
-    const fetchData = async (page: number) => {
-      const limit = 20;
-      const offset = (page - 1) * limit;
-      try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-        const results = response.data.results;
-        setTotalPages(Math.ceil(response.data.count / limit));
-        
-        const detailedData = await Promise.all(
-          results.map(async (pokemon: { url: string }) => {
-            const res = await axios.get(pokemon.url);
-            return {
-              id: res.data.id,
-              name: res.data.name[0].toUpperCase() + res.data.name.slice(1),
-              types: res.data.types.map((t: any) => t.type.name),
-              sprites: res.data.sprites.other["official-artwork"].front_default,
-            };
-          })
-        );
 
-        setPokemonList(detailedData);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
-      }
-    };
-    
-    useEffect(() => {
-      fetchData(currentPage);
-    }, [currentPage]);
-        
+  const fetchData = async (page: number) => {
+    const limit = 20;
+    const offset = (page - 1) * limit;
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+      const results = response.data.results;
+      setTotalPages(Math.ceil(response.data.count / limit));
 
+      const detailedData = await Promise.all(
+        results.map(async (pokemon: { url: string }) => {
+          const res = await axios.get(pokemon.url);
+          return {
+            id: res.data.id,
+            name: res.data.name[0].toUpperCase() + res.data.name.slice(1),
+            types: res.data.types.map((t: any) => t.type.name),
+            sprites: res.data.sprites.other["official-artwork"].front_default,
+          };
+        })
+      );
 
-    return (
-    const fetchData = async (page: number) => {
-      const limit = 20;
-      const offset = (page - 1) * limit;
-      try {
-        const results = response.data.results;
-        const detailedData = await Promise.all(
-          results.map(async (pokemon: { url: string }) => {
-            const res = await axios.get(pokemon.url);
-            return {
-              id: res.data.id,
-              name: res.data.name[0].toUpperCase() + res.data.name.slice(1),
-              types: res.data.types.map((t: string[]) => t.type.name),
-              sprites: res.data.sprites.other["official-artwork"].front_default,
-            };
-          })
-        );
-        setPokemonList(detailedData);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
-      }
-    };
+      setPokemonList(detailedData);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+    }
+  };
+
   useEffect(() => {
-    fetchData(5);
-  }, []);
+    fetchData(currentPage);
+  }, [currentPage]);
 
   return (
     <>
       <div className="bg-white flex flex-col items-center">
-        <BannerIntro />
+        <BannerIntro/>
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2.5">
           {pokemonList.map((pokemon, index) => (
-            <PokemonCard pokemon={pokemon} key={index} />
+            <PokemonCard pokemon={pokemon} key={index}/>
           ))}
         </div>
-        <Pagination totalPages={totalPages} />
+        <Pagination totalPages={totalPages}/>
       </div>
-    );
-  };
+    </>
+  )
+}
 
-  export default HomePage;
-
-
-
-<<<<<<< HEAD
-=======
 export default HomePage;
->>>>>>> 41ce1ff90106ab4cb63ab59d58c6ec4bdce6560d
+
